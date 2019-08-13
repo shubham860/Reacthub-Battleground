31ba9cd5088e70a1b0f011fdb078fe726f92ef42
shubham860 <shubham.chauhan860@gmail.com>
2019-08-14 03:47:02 +0530

    commmit-5

--- reacthub/src/components/Player1.js
+++ reacthub/src/components/Player1.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './css/Player1.css'
 
 
 class Player1 extends React.Component {
@@ -8,7 +9,29 @@ class Player1 extends React.Component {
      return(
        <div>
        {
-         profile1 && <h1>hello</h1>
+         profile1 && <div className='container panel z-depth-5'>
+                      <div className='row'>
+                       <img src={avtar1} alt={avtar1} class="circle responsive-img avtar"  />
+                       </div>
+                       <div classname='info'>
+                         <p className='name1'>{name1}</p>
+                         <h6>{bio1}</h6>
+                         <h5>{location1}</h5>
+                         <div className='row'>
+                           <div className='col s4'>
+                             <h6><ion-icon name="people" id='size'></ion-icon><h6 className='repo'>{followers1} Followers</h6></h6>
+                           </div>
+                           <div className='col s4'>
+                             <h6><ion-icon name="person" id='size'></ion-icon><h6 className='repo'>{following1} Following</h6></h6>
+                           </div>
+                           <div className='col s4'>
+                             <h6><ion-icon name="code" id='size'></ion-icon><h6 className='repo'>{repo1} Repository</h6></h6>
+                           </div>
+                         </div>
+                         <a class="waves-effect waves-light btn-small green darken-1 button" href={link1} target="_blank">See full Profile</a>
+
+                       </div>
+                     </div>
        }
        </div>
      )
--- reacthub/src/components/Player2.js
+++ reacthub/src/components/Player2.js
@@ -6,9 +6,33 @@ class Player2 extends React.Component {
     const {avtar2,username2,name2,bio2,followers2,following2,created2,link2,repo2,location2} = this.props
 
    return(
-      <div>
-         {avtar2 && <img src={avtar2}/> }
-      </div>
+     <div>
+     {
+       profile1 && <div className='container panel z-depth-5'>
+                    <div className='row'>
+                     <img src={avtar1} alt={avtar1} class="circle responsive-img avtar"  />
+                     </div>
+                     <div classname='info'>
+                       <p className='name1'>{name1}</p>
+                       <h6>{bio1}</h6>
+                       <h5>{location1}</h5>
+                       <div className='row'>
+                         <div className='col s4'>
+                           <h6><ion-icon name="people" id='size'></ion-icon><h6 className='repo'>{followers1} Followers</h6></h6>
+                         </div>
+                         <div className='col s4'>
+                           <h6><ion-icon name="person" id='size'></ion-icon><h6 className='repo'>{following1} Following</h6></h6>
+                         </div>
+                         <div className='col s4'>
+                           <h6><ion-icon name="code" id='size'></ion-icon><h6 className='repo'>{repo1} Repository</h6></h6>
+                         </div>
+                       </div>
+                       <a class="waves-effect waves-light btn-small green darken-1 button" href={link1} target="_blank">See full Profile</a>
+
+                     </div>
+                   </div>
+     }
+     </div>
      )
   }
 }
